5457eae0171d72f0507c13d02d21c74abb4b3d49
Yash Gupta <yashg160@gmail.com>
2019-10-22 14:33:23 +0530

    Bugs fixed. No android project

---
+++ README.md
@@ -0,0 +1,73 @@
+# React Native Gifted Fire Chat
+
+React Native Gifted chat is an abstraction over React Native Gifted Chat and React Native Firebase. It provides a simpler API to integrate a chat component into a React Native app, using Firebase Realtime Database to store the chat data.
+
+[Follow author on Github](https://github.com/yashg160)
+[Follow author on LinkedIn](https://www.linkedin.com/in/yash-gupta-575679179/)
+
+## Motivations and Aims
+The idea behind this package was to simplify the process of integrating a chat component in a React Native app. While working on a personal project, I had trouble finding any documentation or resources to do this. So, I decided to create a module myself and share it so that anyone else who might face the same problem as I did, might stumble upon this package.
+
+## Dependencies
+
+- **react-native-gifted-chat** to provide the UI 
+-  **react-native-firebase** to provide firebase connectivity
+
+## IMPORTANT NOTES
+- **This module was tested with react-native@0.59.9 and react-native-firebase@5.5.6. Using react-native-firebase >= 6.0.0 or react-native >= 0.60.0 might introduce breaking changes to your project.**
+ 
+- **This module supports only Android right now. It has not been  tested on iOS. Using this module on iOS may result in unexpected behaviour.**
+
+- **Module will not work without an internet connection(obviously). So remember to check for internet connection**
+
+## Installation
+1.  Install Gifted Chat UI
+`yarn add react-native-gifted-chat` 
+or
+`npm install --save react-native-gifted-chat`
+
+2. Get google-services.json file. Follow the instructions [here](https://rnfirebase.io/docs/v5.x.x/installation/initial-setup) to get your file.
+3. Then follow the instructions for **Android** only setup [here](https://rnfirebase.io/docs/v5.x.x/installation/android). 
+4. After you have completed the initial installation, add the realtime database module to your project. Follow the instructions [here](https://rnfirebase.io/docs/v5.x.x/database/android).
+5. Then run the command `yarn add react-native-gifted-fire-chat` or `npm install --save react-native-gifted-fire-chat`.
+
+After completing the installation, you are ready to use this package.
+
+## Usage
+
+    import GiftedFireChat from 'react-native-gifted-fire-chat';
+	
+	render() {
+		return(
+			<GiftedFireChat
+				senderPhoneNumber='+11234567890'
+				recieverPhoneNumber='+10987654321'
+			/>
+		)
+	}
+
+- **Required Props**: 
+	- **senderPhoneNumber**: *(string)* The current user's phone number along with the country code. Eg: If you are providing a phone number from India, then it should be given as "+911234567890".
+	-	**recieverPhoneNumber**: *(string)* The phone number of the other user. It should be provided in a similar manner as the sender's number.
+	
+
+- **Optional Props**
+	- **renderLoading**(*Component*): An ActivityIndicator or any other React Component to display during loading. Default is <ActivityIndicator> from react-native.
+	- **userId**(*String*): A custom id passed to denote the sender's identity. Make sure that it is unique for each user. Default is the sender's phone number passed in senderPhoneNumber prop.
+	- All other react-native-gifted-chat props. View documentation [here](https://www.npmjs.com/package/react-native-gifted-chat). Quick replies and system messages are not yet supported. 
+	- **userAvatar** *(string)* and **userName** *(string)* can also be supplied as props. Default value for avatar is null and name is 'User Name'
+	- **messages** (*Array*): A custom array of messages can be passed in as prop to display initially before any messages are sent. Each message object should contain a text property, user object(for custom properties) and a time of creation.
+
+## Tips
+1. Use react-native-gifted-chat props [here](https://www.npmjs.com/package/react-native-gifted-chat) to customize the UI to suit your app. You can change almost everything to suit your needs.
+2. To insert a header into your view, it is recommended to use `Header` from `react-native-elements`. View the [documentation](https://react-native-elements.github.io/react-native-elements/docs/header.html).
+3. Use a navigation library such as `react-navigation` or `react-native-router-flux` to move from one screen to another. You can also user the header provided by these libraries if you wish so. View [react-navigation](https://reactnavigation.org/docs/en/getting-started.html). View [react-native-router-flux](https://www.npmjs.com/package/react-native-router-flux).
+4. Provide the senderAvatar prop a download URL for the image. URL can be for a local or network image.
+5. Use `@react-native-community/netinfo1` to check for internet connectivity. View [here](https://www.npmjs.com/package/@react-native-community/netinfo).
+
+## Limitations
+This package is simply an abstraction over two existing modules.
+Right now, only text messages are supported.
+
+## Future Works
+I plan to add support for sending images and videos using Firebase Storage, support for location sharing. I will also look to provide overriding of the onSend prop to allow for greater flexibility. If you want to contribute, ping me and I will add you as a collaborator.
---
+++ index.js
@@ -0,0 +1,182 @@
+import React, { Component } from 'react';
+import { ActivityIndicator } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
+import firebase from 'react-native-firebase';
+
+export default class GiftedFireChat extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            messages: this.props.messages == null ? [] : this.props.messages,
+            loading: true,
+            uniRef: null
+        }
+    }
+
+    TAG = 'GiftedFireChat:';
+    messagesRef = null;
+
+    async setRef() {
+
+        let userNumber = this.props.senderPhoneNumber
+        let personNumber = this.props.recieverPhoneNumber;
+
+        let slicePerson = personNumber.slice(3, 8);
+        let sliceUser = userNumber.slice(3, 8);
+
+        const k1 = Number(slicePerson);
+        const k2 = Number(sliceUser);
+
+        const uniRef = (k1 + k2) * (k1 + k2 + 1) / 2 + k2;
+        this.setState({ uniRef });
+
+        console.log(this.TAG, `Chatting between ${userNumber} and ${personNumber}`);
+        return;
+    }
+
+    async loadMessages(callback) {
+        const { uniRef } = this.state;
+        this.messagesRef = firebase.database().ref(`messages-${uniRef}`);
+        this.messagesRef.off();
+
+        const onReceive = (data) => {
+            const message = data.val();
+            callback({
+                _id: data.key,
+                text: message.text,
+                createdAt: new Date(message.createdAt),
+                user: {
+                    _id: message.user._id,
+                    name: message.user.name,
+                    avatar: message.user.avatar
+                }
+            });
+        };
+        this.messagesRef.limitToLast(20).on('child_added', onReceive);
+        return;
+
+    }
+
+    sendMessage = (message) => {
+        for (let i = 0; i < message.length; i++) {
+            this.messagesRef.push({
+                text: message[i].text,
+                user: message[i].user,
+                createdAt: firebase.database().getServerTime()
+            });
+        }
+    }
+
+    async closeChat() {
+        if (this.messagesRef) {
+            await this.messagesRef.off();
+        }
+    }
+
+    componentDidMount() {
+        this.setRef()
+            .then(() => this.loadMessages(message => {
+                this.setState((previousState) => {
+                    return {
+                        messages: GiftedChat.append(previousState.messages, message)
+                    }
+                });
+            }))
+            .catch(error => {
+                console.log(this.TAG, error);
+            });
+        this.setState({ loading: false });
+    }
+
+    render() {
+        if (this.state.loading) {
+            this.props.renderLoading == null ? <ActivityIndicator /> : this.props.renderLoading
+        }
+
+        return (
+            <View style={{ flex: 1 }}>
+
+                <GiftedChat
+                    renderLoading={() => this.props.renderLoading == null ? <ActivityIndicator /> : this.props.renderLoading}
+                    messages={this.state.messages}   //messages can be passed as prop
+                    onSend={message => this.sendMessage(message)}   //Options for overriding
+                    user={{
+                        _id: this.props.userId == null ? this.props.senderPhoneNumber : this.props.userId,
+                        name: this.props.userName == null ? 'User Name' : this.props.userName,
+                        avatar: this.props.userAvatar == null ? null : this.props.userAvatar
+                    }}
+                    text={this.props.text}
+                    placeholder={this.props.placeholder}
+                    messageIdGenerator={this.props.messageIdGenerator}
+                    alwaysShowSend={this.props.alwaysShowSend}
+                    locale={this.props.locale}
+                    timeFormat={this.props.timeFormat}
+                    dateFormat={this.props.dateFormat}
+                    isAnimated={this.props.isAnimated}
+                    loadEarlier={this.props.loadEarlier}
+                    isLoadingEarlier={this.props.isLoadingEarlier}
+                    renderLoadEarlier={this.props.renderLoadEarlier}
+                    renderAvatar={this.props.renderAvatar}
+                    showUserAvatar={this.props.showUserAvatar}
+                    showAvatarForEveryMessage={this.props.showAvatarForEveryMessage}
+                    onPressAvatar={this.props.onPressAvatar}
+                    onLongPressAvatar={this.props.onLongPressAvatar}
+                    renderAvatarOnTop={this.props.renderAvatarOnTop}
+                    renderBubble={this.props.renderBubble}
+                    renderSystemMessage={this.props.renderSystemMessage}
+                    onLongPress={this.props.onLongPress}
+                    inverted={this.props.inverted}
+                    renderUsernameOnMessage={this.props.renderUsernameOnMessage}
+                    renderMessage={this.props.renderMessage}
+                    renderMessageText={this.props.renderMessageText}
+                    /* renderMessageImage={this.props.renderMessageImage}
+                    renderMessageVideo={this.props.renderMessageVideo} */
+                    videoProps={this.props.videoProps}
+                    lightboxProps={this.props.lightboxProps}
+                    isCustomViewBottom={this.props.isCustomViewBottom}
+                    renderCustomView={this.props.renderCustomView}
+                    renderDay={this.props.renderDay}
+                    renderTime={this.props.renderTime}
+                    renderFooter={this.props.renderFooter}
+                    renderChatFooter={this.props.renderChatFooter}
+                    renderInputToolbar={this.props.renderInputToolbar}
+                    renderComposer={this.props.renderComposer}
+                    renderActions={this.props.renderActions}
+                    renderSend={this.props.renderSend}
+                    renderAccessory={this.props.renderAccessory}
+                    onPressActionButton={this.props.onPressActionButton}
+                    bottomOffset={this.props.bottomOffset}
+                    minInputToolbarHeight={this.props.minInputToolbarHeight}
+                    listViewProps={this.props.listViewProps}
+                    textInputProps={this.props.textInputProps}
+                    keyboardShouldPersistTaps={this.props.keyboardShouldPersistTaps}
+                    onInputTextChanged={this.props.onInputTextChanged}
+                    maxInputLength={this.props.maxInputLength}
+                    parsePatterns={this.props.parsePatterns}
+                    extraData={this.props.extraData}
+                    minComposerHeight={this.props.minComposerHeight}
+                    maxComposerHeight={this.props.maxComposerHeight}
+                    scrollToBottom={this.props.scrollToBottom}
+                    scrollToBottomComponent={this.props.scrollToBottomComponent}
+                    scrollToBottomOffset={this.props.scrollToBottomOffset}
+                    scrollToBottomStyle={this.props.scrollToBottomStyle}
+                    alignTop={this.props.alignTop}
+                    /* onQuickReply={this.props.onQuickReply}
+                    renderQuickReplies={this.props.renderQuickReplies}
+                    quickReplyStyle={this.props.quickReplyStyle}
+                    renderQuickReplySend={this.props.renderQuickReplySend} */
+                    shouldUpdateMessage={this.props.shouldUpdateMessage}
+
+                />
+            </View>
+
+        );
+    }
+
+    componentWillUnmount() {
+        this.closeChat()
+            .then(() => console.log(this.TAG, 'Chat closed'))
+            .catch(error => console.log(this.TAG, error));
+    }
+}
